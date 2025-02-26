a533e50d6581a67c24f1cedc35dca93228539c21
Vladimir Riha <vladimir.riha@oracle.com>
2025-02-26 12:57:57 +0100

    Improve components

--- client/src/components/Event.tsx
+++ client/src/components/Event.tsx
@@ -1,30 +1,84 @@
 import React from "react";
-import { DateRecord } from "./Date";
+import { AnswerType, Timestamp, User, UserRecord } from "./User";
 
 type EventProps = {
-  location: "Hradec Králové";
-  id: 1;
-  title: "Fotbalový zápas";
+  location: string;
+  id: number;
+  title: string;
   dates: DateRecord[];
 };
 
+export type DateRecord = {
+  records: UserRecord[];
+  timestamp: number;
+};
 
-export const Event: React.FC<EventProps> = ({ location }: EventProps, { title }: EventProps, { dates }: EventProps) => {
+type UserName = string;
+
+type UserEntry = {
+  name: UserName;
+  votes: Map<Timestamp, { timestamp: Timestamp; answer?: AnswerType }>;
+};
+
+type UserEntryArray = {
+  name: UserName;
+  votes: Array<{ timestamp: Timestamp; answer?: AnswerType }>;
+};
+
+const getUsersFromDates = (dates: DateRecord[]): { dates: number[]; votes: UserEntryArray[] } => {
+  // ocekavejme unikatni jmeno pro zjednoduseni
+  const allVotes = new Map<UserName, UserEntry>();
+  const allDates: number[] = [];
+
+  dates.forEach((date) => {
+    allDates.push(date.timestamp);
+    date.records.forEach((userRecord) => {
+      const userVotes: UserEntry = allVotes.get(userRecord.name) || { name: userRecord.name, votes: new Map() };
+      userVotes.votes.set(date.timestamp, { timestamp: date.timestamp, answer: userRecord.answer });
+      allVotes.set(userRecord.name, userVotes);
+    });
+  });
+
+  // dorovnat data
+  const votes = Array.from(allVotes.values());
+  votes.forEach((userVote) => {
+    if (userVote.votes.size !== allDates.length) {
+      allDates.forEach((date) => {
+        if (!userVote.votes.has(date)) {
+          userVote.votes.set(date, { timestamp: date });
+        }
+      });
+    }
+  });
+
+  // nevime jiste poradi dle data, lepe seradit
+  return {
+    dates: allDates.sort((a, b) => a - b),
+    votes: votes.map((v) => {
+      const sortedVotes = Array.from(v.votes.values()).sort((a, b) => a.timestamp - b.timestamp);
+      return {
+        name: v.name,
+        votes: sortedVotes
+      };
+    })
+  };
+};
+export const Event: React.FC<EventProps> = ({ location, title, dates }: EventProps) => {
+  const users = getUsersFromDates(dates);
+
   return (
     <>
       <h3> Místo události: {location} </h3>
       <h3> Popis události: {title} </h3>
       <table>
         <thead>
-          <th>Osoba</th>
-          // @ts-ignore
-          <th></th>
-
+          <tr>
+            <th>Osoba</th>
+            {users.dates.map((date) => (<th key={date}>{new Date(date).toLocaleDateString()}</th>))}
+          </tr>
         </thead>
         <tbody>
-
-          ???????????????????????????????
-
+          {users.votes.map((vote) => <User key={vote.name} name={vote.name} votes={vote.votes} />)}
         </tbody>
       </table>
     </>
--- client/src/components/User.tsx
+++ client/src/components/User.tsx
@@ -1,25 +1,23 @@
 import React from "react";
 
+export type Timestamp = number;
+export type AnswerType = "yes" | "no" | "if-needed";
+export type VotesType = Array<{ timestamp: Timestamp; answer?: AnswerType }>;
 export type UserRecord = {
   name: string;
   answer: "yes" | "no" | "if-needed";
 };
 
-export type DateRecord = {
-  records: UserRecord[];
-  timestamp: number;
-
+export type UserProps = {
+  name: string;
+  votes: VotesType;
 };
 
-export const User: React.FC<UserRecord> = ({name}:UserRecord, {answer}:UserRecord )=>
-{
-    return (<tr>
-        <td>{name}</td>
-        <td>{answer}</td>
+export const User: React.FC<UserProps> = ({ name, votes }) => {
+  return (
+    <tr>
+      <td>{name}</td>
+      {votes.map((v) => <td key={v.timestamp}>{v.answer || "-"}</td>)}
     </tr>
-        );
-
+  );
 };
-
-
-
